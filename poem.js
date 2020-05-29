9233149d10cb82131e46216fbb626201b0a0c242
Deyby Rodriguez <deybyr647@gmail.com>
2020-05-29 16:30:28 -0400

    Create poem.js

---
+++ poem.js
@@ -0,0 +1,16 @@
+const author = {firstName: "Deyby", lastName: "Rodriguez"}
+
+let message = `This poem is by ${firstName} ${lastName}. Enjoy`
+
+let poem = ["Roses Are Red", "Violets Are Blue", "I love JavaScript", "I hope you do too!"]
+
+let poemOut = () => {
+  let i;
+  for(i=0; i <= poem.length; i++){
+    console.log(poem[i])
+    }
+  console.log(message);
+}  
+
+poemOut();
+
