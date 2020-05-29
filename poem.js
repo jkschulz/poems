49c58bb8b2059e50a5f65786626b1a08fbcb5fcc
Deyby Rodriguez <deybyr647@gmail.com>
2020-05-29 16:31:29 -0400

    Bug fixes to my poem code

--- poem.js
+++ poem.js
@@ -1,6 +1,6 @@
 const author = {firstName: "Deyby", lastName: "Rodriguez"}
 
-let message = `This poem is by ${firstName} ${lastName}. Enjoy`
+let message = `This poem is by ${author.firstName} ${author.lastName}. Enjoy`
 
 let poem = ["Roses Are Red", "Violets Are Blue", "I love JavaScript", "I hope you do too!"]
 
